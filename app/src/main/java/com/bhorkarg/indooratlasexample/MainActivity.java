4f8614cb96b2851a553e711e6892520c1cc3fa29
Gaurav Bhorkar <gaurav.bhorkar@aalto.fi>
2016-02-22 18:34:29 +0200

    Commented code

--- app/src/main/java/com/bhorkarg/indooratlasexample/MainActivity.java
+++ app/src/main/java/com/bhorkarg/indooratlasexample/MainActivity.java
@@ -13,11 +13,13 @@ public class MainActivity extends AppCompatActivity {
 
     IALocationManager mLocationManager;
 
+    //Listener for receiving location updates
     IALocationListener mLocationListener = new IALocationListener() {
         @Override
         public void onLocationChanged(IALocation iaLocation) {
+            //Location updates will be received here
             TextView txtLoc = (TextView) findViewById(R.id.txtLocation);
-            txtLoc.setText(String.valueOf(iaLocation.getLatitude() + ", " + iaLocation.getLongitude()));
+            txtLoc.setText(iaLocation.getLatitude() + ", " + iaLocation.getLongitude());
         }
 
         @Override
@@ -32,16 +34,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mLocationManager = IALocationManager.create(this);
+        mLocationManager = IALocationManager.create(this); //create a IALocationManager
     }
 
     protected void onResume() {
         super.onResume();
+        //Start requesting location updates. Specify the listener.
         mLocationManager.requestLocationUpdates(IALocationRequest.create(), mLocationListener);
 
     }
 
     protected void onPause() {
+        //Stop receiving location updates when app paused.
         mLocationManager.removeLocationUpdates(mLocationListener);
         super.onPause();
     }
